65f2b962d09171260a46536306a5d2ca1caf08d0
xkostrna <kostrnatomas@gmail.com>
2022-04-20 09:08:22 +0200

    FEAT : jersey server

--- src/main/java/sk/stuba/fei/uim/vsa/School.java
+++ src/main/java/sk/stuba/fei/uim/vsa/School.java
@@ -1,5 +1,44 @@
 package sk.stuba.fei.uim.vsa;
 
+import jakarta.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class School {
+
+    public static final Logger LOGGER = Logger.getLogger(School.class.getName());
+    public static final String BASE_URI = "http://localhost/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig config = ResourceConfig.forApplicationClass(SchoolApplication.class);
+        URI baseUri = UriBuilder.fromUri(BASE_URI).port(8080).build();
+        LOGGER.info("Starting Jetty server...");
+        LOGGER.info("Server listening on http://localhost:8080/");
+        return GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+    }
+
+    public static void main(String[] args) {
+        try {
+            final HttpServer server = startServer();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    System.out.println("Shutting down the application...");
+                    server.shutdown();
+                    System.out.println("Exiting...");
+                } catch (Exception e) {
+                    LOGGER.log(Level.SEVERE, null, e);
+                }
+            }));
+            System.out.printf("Application started.%nStop the application using CTRL+C%n");
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.SEVERE, null, e);
+        }
+    }
 
 }
--- src/main/java/sk/stuba/fei/uim/vsa/SchoolApplication.java
+++ src/main/java/sk/stuba/fei/uim/vsa/SchoolApplication.java
@@ -1,5 +1,21 @@
 package sk.stuba.fei.uim.vsa;
 
-public class SchoolApplication {
+import jakarta.ws.rs.core.Application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SchoolApplication extends Application {
+
+    static final Set<Class<?>> appClasses = new HashSet<>();
+
+    static {
+//        appClasses.add();
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return appClasses;
+    }
 
 }
